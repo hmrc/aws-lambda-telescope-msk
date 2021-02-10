d146dd575fc0ba69992e13c70dbd8eb0e6b79dee
Samuel Allwright <71259281+samallwright@users.noreply.github.com>
2021-02-10 16:48:46 +0000

    TEL-2242 WIP

--- telemetry/telescope_msk/copies.py
+++ telemetry/telescope_msk/copies.py
@@ -1,41 +1,19 @@
-import confluent_kafka
-from telemetry.telescope_msk.msk import get_default_bootstrap_servers
+################################### VVVVVVV from https://github.com/confluentinc/confluent-kafka-python/blob/master/examples/list_offsets.py
+    print("%-50s  %9s  %9s" % ("Topic [Partition]", "Committed", "Lag"))
+    print("=" * 72)
 
-
-def list_offsets():
-    # bootstrap_servers = get_default_bootstrap_servers()
-    bootstrap_servers = 'localhost:9092'
-    from confluent_kafka import Consumer
-
-    consumer = Consumer({
-        'bootstrap.servers': bootstrap_servers,
-        'group.id': 'metrics',
-
-    })
-
-    topic = 'metrics'
-
-    try:
+    for topic in sys.argv[3:]:
+        # Get the topic's partitions
         metadata = consumer.list_topics(topic, timeout=10)
-        print(metadata.topics)
         if metadata.topics[topic].error is not None:
             raise confluent_kafka.KafkaException(metadata.topics[topic].error)
 
         # Construct TopicPartition list of partitions to query
         partitions = [confluent_kafka.TopicPartition(topic, p) for p in metadata.topics[topic].partitions]
-        print(partitions)
-
-
-        for partition in partitions:
-            print(consumer.get_watermark_offsets(partition))
-
-
-
-
 
         # Query committed offsets for this group and the given partitions
         committed = consumer.committed(partitions, timeout=10)
-        print(committed)
+
         for partition in committed:
             # Get the partitions low and high watermark offsets.
             (lo, hi) = consumer.get_watermark_offsets(partition, timeout=10, cached=False)
@@ -58,6 +36,29 @@ def list_offsets():
             print("%-50s  %9s  %9s" % (
                 "{} [{}]".format(partition.topic, partition.partition), offset, lag))
 
-    except Exception as e:
-        consumer.close()
+    consumer.close()
 
+
+###is this how to glean all metrics? check metrics available to partition and consumer?
+
+    ##################### or VVVVVVVV from https://www.confluent.io/blog/introduction-to-apache-kafka-for-python-programmers/
+    consumer.subscribe(metadata) ##or should this be ([metadata]) or (['metadata'])
+
+    try:
+        while True:
+            msg = consumer.poll(0.1)
+            if msg is None:
+                continue
+            elif not msg.error():
+                print('Received message: {0}'.format(msg.value()))
+            elif msg.error().code() == KafkaError._PARTITION_EOF:
+                print('End of partition reached {0}/{1}'
+                      .format(msg.topic(), msg.partition()))
+            else:
+                print('Error occured: {0}'.format(msg.error().str()))
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        consumer.close()
