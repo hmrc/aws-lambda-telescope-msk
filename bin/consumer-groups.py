fe55eb89245fe9661ab687af3d69a2329c9a5adf
Samuel Allwright <71259281+samallwright@users.noreply.github.com>
2021-02-19 12:36:29 +0000

    TEL-2242 Add methods to publish metrics group/topic/partition/metrics/value to graphite

--- bin/consumer-groups.py
+++ bin/consumer-groups.py
@@ -2,11 +2,14 @@
 
 import logging
 
+from confluent_kafka.admin import GroupMetadata
+
 from telemetry.telescope_msk.adminapi import list_consumer_groups, create_admin_client, list_consumer_groups_excluding
 from telemetry.telescope_msk.cli import cli
 from telemetry.telescope_msk.logger import create_app_logger
 from telemetry.telescope_msk.consumer import list_offsets, get_consumer
 from telemetry.telescope_msk.msk import get_plaintext_bootstrap_servers
+from telemetry.telescope_msk.send_graphyte_message import publish_asgs_to_graphite
 
 
 # what do we hope to achieve?
@@ -21,7 +24,7 @@ from telemetry.telescope_msk.msk import get_plaintext_bootstrap_servers
 #   Make it run continuously in a ecs task
 
 
-def main(log_level: str = logging.DEBUG) -> None:
+def main(log_level: str = logging.DEBUG, graphite_host="graphite") -> None:
     bootstrap_servers = get_plaintext_bootstrap_servers()
     create_app_logger(log_level)
     consumer_groups = list_consumer_groups_excluding(create_admin_client(bootstrap_servers),
@@ -30,13 +33,23 @@ def main(log_level: str = logging.DEBUG) -> None:
                                                       'amazon.msk.canary.group.broker-1'])
 
     for group in consumer_groups:
-        print(f'\n\n{group.id}')
+
         consumer = get_consumer(bootstrap_servers, group.id)
-        print("\n".join(list_offsets(consumer)))
+        metrics = list_offsets(consumer)
         consumer.close()
 
-        '{consumer_group}.{topic_name}.partition_{partition}-{high/low/lag}'
-        #logstash.logs.partition_0.high.$high_watermark
+        for metric in metrics:
+            keys = ['high', 'low', 'lag']
+            for key in keys:
+                value = metric[key]
+                # EG: logstash.logs.partition_0.high.$high_watermark
+                print(create_metric_key(metric, group.id, key)+f"={value}")
+                publish_asgs_to_graphite(create_metric_key(metric, group.id, key),value, graphite_host)
+
+
+def create_metric_key(metric: dict, group_id: str, key: str) -> str:
+    return f"{group_id}.{metric['topic_name']}.partition_{metric['partition_id']}.{key}"
+
 
 if __name__ == '__main__':
     cli(main)
--- telemetry/telescope_msk/__init__.py
+++ telemetry/telescope_msk/__init__.py
@@ -1,2 +1,3 @@
 APP_NAME = 'telescope-msk'
 APP_VERSION = "0.1.0"
+METRICS_PREFIX = "telemetry.telescope.msk"
