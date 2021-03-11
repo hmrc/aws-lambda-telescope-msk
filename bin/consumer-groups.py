56d56424a5307de46060c22dc84502c6a2ecb0e4
Corin Wilkins <1826895+CorinWilkins@users.noreply.github.com>
2021-03-11 11:55:02 +0000

    TEL-2242 Cleansup code ahead of review.

--- bin/consumer-groups.py
+++ bin/consumer-groups.py
@@ -8,16 +8,6 @@ from telemetry.telescope_msk.logger import create_app_logger
 from telemetry.telescope_msk.consumer import get_metrics_for_groups_and_topics
 from telemetry.telescope_msk.msk import get_plaintext_bootstrap_servers
 
-# what do we hope to achieve?
-# Overall grab consumer lag for all consumer groups in a grafana dashboard
-# / Return a list of consumer groups excluding aws msk
-# / filter out __consumer_offsets and __aws_msk_etc
-# / Get the metrics for each partition in group
-# / Transform into a string  something like ${partition}-${topic_name}-${metric_name}, Check with Vitor
-# / Push to graphite
-# / Build a grafana dashboard to display it.
-# / Make available to consumer.py
-#   Make it run continuously in a ecs task
 
 
 def main(log_level: str = logging.DEBUG, graphite_host="graphite") -> None:
