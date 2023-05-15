89b6594ce24744036f08df913a6bf6a226a7a837
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2023-05-15 15:20:05 +0100

    TEL-3758: incorporate graphyte into project
    * team decided to move the small bit of python code locally instead of dealing with awkward wheel dependencies
    
    Co-authored-by: Rizina Khatun <66684341+rizinaa99@users.noreply.github.com>

--- src/telescope_msk/send_graphyte_message.py
+++ src/telescope_msk/send_graphyte_message.py
@@ -1,5 +1,6 @@
-import graphyte
 from telescope_msk.app_info import METRICS_PREFIX
+from telescope_msk.graphyte import init
+from telescope_msk.graphyte import send
 from telescope_msk.logger import get_app_logger
 
 
@@ -7,5 +8,5 @@ logger = get_app_logger()
 
 
 def publish_kafka_to_graphite(path, metrics, graphite_host):
-    graphyte.init(graphite_host, prefix=METRICS_PREFIX)
-    graphyte.send(path, metrics)
+    init(graphite_host, prefix=METRICS_PREFIX)
+    send(path, metrics)
